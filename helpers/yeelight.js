4881a7f5ea95af7fd1a0b6c9ad345bb882077ad6
Thiago Araujo <thirauj@br.ibm.com>
2018-11-20 18:07:01 -0200

    Add light class

--- helpers/yeelight.js
+++ helpers/yeelight.js
@@ -1,2 +1,19 @@
 (function () {
+	let y = require("yeelight-awesome");
+
+    module.exports = function Constructor (payload) {
+		return {
+			"yeelight": new y.Yeelight(payload),
+			"connect": function () {
+				return new Promise((resolve, reject) => {
+					this.yeelight.connect();
+
+					this.yeelight.on("connected", () => {
+						console.log("fgoi");
+					});
+				});
+			}
+		}
+	}
+	
 }());
